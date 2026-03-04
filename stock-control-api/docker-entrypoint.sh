0492e72c4cb0f060421aeee2ae369feeb442c9b7
Alex Junior <alexjunior2018@gmail.com>
2026-03-04 01:52:50 -0300

    chore: update build plugins and devtools configuration

---
+++ stock-control-api/docker-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+./mvnw spring-boot:run -Dspring-boot.run.jvmArguments="-Dserver.address=0.0.0.0" &
+SPRING_PID=$!
+
+echo "Watching for changes using polling mode..."
+last_hash=$(find /work/src/main -type f -exec ls -lT {} + | md5sum)
+
+while true; do
+  sleep 2
+  current_hash=$(find /work/src/main -type f | xargs md5sum)
+
+  if [ "$current_hash" != "$last_hash" ]; then
+    echo "Change detected! Recompiling..."
+    ./mvnw compile -o -DskipTests
+    last_hash=$current_hash
+  fi
+done
