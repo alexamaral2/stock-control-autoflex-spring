01c148f18d5f45db6664958c49319dd96e85d495
Alex Junior <alexjunior2018@gmail.com>
2026-03-04 02:18:47 -0300

    ajustando docker rebuild livecode

--- stock-control-api/docker-entrypoint.sh
+++ stock-control-api/docker-entrypoint.sh
@@ -4,15 +4,15 @@
 SPRING_PID=$!
 
 echo "Watching for changes using polling mode..."
-last_hash=$(find /work/src/main -type f -exec ls -lT {} + | md5sum)
+last_hash=$(find /work/src/main -type f -exec md5sum {} +)
 
 while true; do
   sleep 2
-  current_hash=$(find /work/src/main -type f | xargs md5sum)
+  current_hash=$(find /work/src/main -type f -exec md5sum {} +)
 
   if [ "$current_hash" != "$last_hash" ]; then
     echo "Change detected! Recompiling..."
     ./mvnw compile -o -DskipTests
-    last_hash=$current_hash
+    last_hash="$current_hash"
   fi
 done
